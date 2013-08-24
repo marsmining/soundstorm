e38dd3e18633bbd4ad27b1d3bdadacb08698a0d8
marsmining <marsmining@gmail.com>
2013-08-24 15:39:01 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject soundstorm "0.1.0-SNAPSHOT"
+  :description "Soundstorm - Awesomize SoundCloud"
+  :url "http://ockhamsolutions.de/soundstorm"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [ch.qos.logback/logback-classic "1.0.10"]
+                 [core.async "0.1.0-SNAPSHOT"]])
---
+++ test/soundstorm/core_test.clj
@@ -0,0 +1,7 @@
+(ns soundstorm.core-test
+  (:require [clojure.test :refer :all]
+            [soundstorm.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
